a715d81acd2444b80351dfac996ab5acb91d4910
thebloggerboy <thebloggerboy06@gmail.com>
2025-07-13 19:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # rozy-gateway-bot/config.py
-
-WORKER_BOT_USERNAME = "RubyWorkerBot"
-MAIN_CHANNEL_LINK = "https://t.me/Primium_Links" # <-- यहाँ अपने मेन चैनल का लिंक डालें
+WORKER_BOT_USERNAME = "RubyWorkerBot" # Ruby का यूजरनेम
+ADMIN_IDS = [6056915535]
+FORCE_SUB_CHANNELS = [
+    {"chat_id": -1002599545967, "name": "Join 1", "invite_link": "https://t.me/+p2ErvvDmitZmYzdl"},
+    {"chat_id": -1002391821078, "name": "Join 2", "invite_link": "https://t.me/+T4LO1ePja_I5NWQ1"}
+]
+# Rozy को फाइल डेटा की ज़रूरत नहीं है
