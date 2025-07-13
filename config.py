6472a4deaa55d70b5d73edd7dae43f2bf8cf1793
thebloggerboy <thebloggerboy06@gmail.com>
2025-07-13 20:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # rozy-gateway-bot/config.py
 WORKER_BOT_USERNAME = "RubyWorkerBot" # Ruby का यूजरनेम
 ADMIN_IDS = [6056915535]
+MAIN_CHANNEL_LINK = "https://t.me/+wIn3ScSvBnoyZDVl"
 FORCE_SUB_CHANNELS = [
     {"chat_id": -1002599545967, "name": "Join 1", "invite_link": "https://t.me/+p2ErvvDmitZmYzdl"},
     {"chat_id": -1002391821078, "name": "Join 2", "invite_link": "https://t.me/+T4LO1ePja_I5NWQ1"}
