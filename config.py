6e03f7df99c8bcaa6955610a45793fd75a6c3a8c
thebloggerboy <thebloggerboy06@gmail.com>
2025-07-17 23:34:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 # rozy-gateway-bot/config.py
-import os
-
-# --- टोकन को एनवायरनमेंट से लें ---
-TOKEN = os.environ.get("GATEWAY_BOT_TOKEN")
 
 # 1. आपकी एडमिन ID
 ADMIN_IDS = [6056915535] 
 
 # 2. वर्कर बॉट का यूजरनेम
-WORKER_BOT_USERNAME = "RubyWorkerBot" # <-- यहाँ Ruby बॉट का सही यूजरनेम डालें
+WORKER_BOT_USERNAME = "RubyWorkerBot" # <-- यहाँ Ruby बॉट का सही यूजरनेम डालें (बिना @)
 
 # 3. मेन चैनल का लिंक
 MAIN_CHANNEL_LINK = "https://t.me/Primium_Links" # <-- यहाँ अपने मेन चैनल का लिंक डालें
